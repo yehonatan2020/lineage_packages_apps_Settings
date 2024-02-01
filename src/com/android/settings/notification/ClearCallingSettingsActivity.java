0d9f8116ab7d0f66b0db755db047fe128e525582
yehonatan2020 <yehonatan@yandex.ru>
2024-02-01 22:38:28 +0000

    Settings: Import Clear Calling

---
+++ src/com/android/settings/notification/ClearCallingSettingsActivity.java
@@ -0,0 +1,11 @@
+
+package com.statix.android.settings.notification;
+
+import com.android.settings.SettingsActivity;
+
+public class ClearCallingSettingsActivity extends SettingsActivity {
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return ClearCallingSettings.class.getName().equals(fragmentName);
+    }
+}
